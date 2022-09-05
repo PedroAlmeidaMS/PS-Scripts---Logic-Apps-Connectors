46a4ca716e5ff4e0e242ad7260fa5138569a4673
PedroAlmeidaMS <112867201+PedroAlmeidaMS@users.noreply.github.com>
2022-09-05 12:04:09 +0100

    Add files via upload
    First version of the Powershell scripts to List Connections and Connectors for Logic Apps Consumption and Standard.

---
+++ Cloud_ListLogicAppStandardConnectors.ps1
@@ -0,0 +1,99 @@
+﻿$resourceGroupName = 'YOUR RESOURCE GROUP'
+
+$path = '/home/' + $Env:USER
+##Do not change the Connections file name
+$pathFile = $path + '/connections.json'
+$csvFilepath = $path + '/OrphanedConnectorsStandard.csv'
+
+#use the collection to build up objects for the table
+$connectorDictionary = New-Object "System.Collections.Generic.Dictionary``2[System.String,System.Object]" 
+
+##Searches for the API Connectors in the RG. Adds them to the table
+##Convert to JSON when debugging, to see results. For script execution, remove the Convert, else the ForEach will not work properly.
+Write-Host ''
+Write-Host 'Looking up API Connectors in Resource Group' $resourceGroupName
+Write-Host 'Will classify as Connected or in Error, does not mean it is used!!'
+$resources = Get-AzResource -ResourceGroupName $resourcegroupName -ResourceType Microsoft.Web/connections -ExpandProperties #|  ConvertTo-Json
+$resources | ForEach-Object { 
+
+    ##Classifying Connector Type
+    if($_.Kind -eq 'V1')
+    {   Write-Host 'Found connector:' ($_.Properties.api.psobject.Properties | Where-Object {$_.Name -eq "displayName"}).Value '| Connector Name: '$_.Name '|  Consumption Managed Connector | Status:' $_.Properties.overallStatus '| connected user:' $_.Properties.authenticatedUser.psobject.Properties.value }
+    else
+    { 
+        ##Adds the resource to the Dictionary
+        ##Add only Standard Connectors
+        Write-Host 'Found Connector:' ($_.Properties.api.psobject.Properties | Where-Object {$_.Name -eq "displayName"}).Value '| Connector Name: '$_.Name '| Standard Managed Connector | Status:' $_.Properties.overallStatus '| connected user:' $_.Properties.authenticatedUser.psobject.Properties.value 
+        
+        $azureConnector = New-Object -TypeName psobject
+        $azureConnector | Add-Member -MemberType NoteProperty -Name 'IsUsed' -Value 'FALSE'
+        $azureConnector | Add-Member -MemberType NoteProperty -Name 'Id' -Value $_.Id
+        $azureConnector | Add-Member -MemberType NoteProperty -Name 'name' -Value $_.Name
+        $azureConnector | Add-Member -MemberType NoteProperty -Name 'Status' -Value $_.Properties.overallStatus
+        if([bool]($myObject.PSobject.Properties.name -match "Deprecated"))
+        { $azureConnector | Add-Member -MemberType NoteProperty -Name 'IsDeprecated' -Value $_.Properties.Deprecated }
+
+        $connectorDictionary.Add($_.Name, $azureConnector)  
+    } 
+}
+
+##Checking for all Logic Apps
+Write-Host ''
+Write-Host 'Looking up Logic Apps Standard'
+$resources = Get-AzResource -ResourceGroupName $resourcegroupName -ResourceType Microsoft.Web/sites -ExpandProperties
+$resources | ForEach-Object {
+    ##If kind does not contain WORKFLOWAPP, it's not a Logic App, it's only a Function App.    
+    if($_.Kind.Contains('workflowapp'))
+    {
+        Write-Host 'Logic App' $_.Name
+        ##Iterate to find Workflows         
+        Write-Host ''
+        Write-Host 'Looking up Workflows'
+		$workflow = Get-AzResource -ApiVersion "2022-03-01" -Name $_.Name -ResourceGroupName $resourceGroupName -ResourceType "Microsoft.Web/sites/workflows/" -ExpandProperties 
+		$workflow | ForEach-Object { Write-Host $_.Name  'type' $_.Kind }    
+
+        Write-Host ''
+        Write-Host 'Checking connectors in LogicApp'
+        ##Get Function App properties
+        $functionapp = Get-AzFunctionApp -Name $_.Name -ResourceGroupName $resourcegroupName 
+
+        ##Context needs to be set to access Az Storage, using ConnectionString from Function App
+        $context = New-AzStorageContext -ConnectionString $functionapp.ApplicationSettings.WEBSITE_CONTENTAZUREFILECONNECTIONSTRING
+        $fileshare = Get-AzStorageShare -Name $functionapp.ApplicationSettings.WEBSITE_CONTENTSHARE -Context $context
+        
+        write-Host 'Logic App' $_.Name ' uses the following connector/s:' 
+        foreach ($fileName in $fileshare.Name)
+        {
+            ##We need to download the file, it does not allow us to work in memory
+            Get-AzStorageFileContent -ShareName $fileName -Path '/site/wwwroot/connections.json' -Context $context -Force -Destination $path
+            $json = Get-Content -Raw -Path $pathFile | Out-String | ConvertFrom-Json
+            foreach($obj in $json.managedApiConnections.psobject.properties)
+            { 
+                write-Host $obj.Value.connection.id '| Is Deprecated? ' ($obj.Value.connection.name.ToString().ToUpper() -contains 'DEPRECATED') 
+                #Check if connector is in the connector dictionary
+                $connectorIdLower = $obj.Value.connection.id.ToString().Replace("@{","").Replace("}","").ToLower()
+				if($connectorDictionary.ContainsKey($connectorIdLower))
+                {
+                    #Mark connector as being used                        
+                    $matchingConnector = $connectorDictionary[$connectorIdLower]
+                    $matchingConnector.IsUsed = 'TRUE'
+                    $connectorDictionary[$connectorIdLower] = $matchingConnector 
+                }
+            }
+        }
+    }
+}
+
+Write-Host ''
+Write-Host 'Orphaned API Connectors'
+$connectorDictionary.Values | ForEach-Object{
+    $azureConnector = $_
+
+    Write-Host $azureConnector.name ' : ' $azureConnector.IsUsed
+    if($azureConnector.IsUsed -eq 'FALSE')
+    {
+        Write-Host $azureConnector.name ' : is an orphan  | ID: ' $azureConnector.Id
+    }
+} 
+
+$connectorDictionary.Values | Export-Csv -Path $csvFilepath
---
+++ Local_ListLogicAppsConsumptionConnectors.ps1
@@ -0,0 +1,90 @@
+﻿$subscriptioName = 'YOUR SUBSCRIPTION'
+$resourceGroupName = 'YOUR RESOURCE GROUP'
+
+## LOGIN to your Azure Account
+az login
+az account set --subscription $subscriptioname
+$path = '/Temp/' + $Env:USER
+$csvFilepath = $path + '/OrphanedConnectorsConsumption.csv'
+
+#Get Resource Group Info to be used in Az REST command
+$resourceGroup = Get-AzResourceGroup -Name $resourceGroupName
+$resourceGroupPath = $resourceGroup.ResourceId
+Write-Host 'Resource Group Path: '  $resourceGroupPath
+
+#use the collection to build up objects for the table
+$connectorDictionary = New-Object "System.Collections.Generic.Dictionary``2[System.String,System.Object]" 
+######
+Write-Host ''
+Write-Host 'Looking up API Connectors'
+$resourceName = ''
+$resources = Get-AzResource -ResourceGroupName $resourcegroupName -ResourceType Microsoft.Web/connections -ExpandProperties 
+$resources | ForEach-Object {    
+    if($_.Kind -eq 'V1')
+    {
+        Write-Host 'Connector: ' ($_.Properties.api.psobject.Properties | Where-Object {$_.Name -eq "displayName"}).Value  ' | Connector Name: '$_.Name ' | Status:' $_.Properties.overallStatus '| connected user:' $_.Properties.authenticatedUser.psobject.Properties.value 
+        $resourceIdLower = $_.id.ToLower()
+        ##Add members to the dictionary
+        $azureConnector = New-Object -TypeName psobject
+        $azureConnector | Add-Member -MemberType NoteProperty -Name 'IsUsed' -Value 'FALSE'
+        $azureConnector | Add-Member -MemberType NoteProperty -Name 'Id' -Value $_.Id
+        $azureConnector | Add-Member -MemberType NoteProperty -Name 'name' -Value $_.Name
+        $azureConnector | Add-Member -MemberType NoteProperty -Name 'Status' -Value $_.Properties.overallStatus
+        if([bool]($myObject.PSobject.Properties.name -match "Deprecated"))
+        { $azureConnector | Add-Member -MemberType NoteProperty -Name 'IsDeprecated' -Value $_.Properties.Deprecated }
+
+        $connectorDictionary.Add($resourceIdLower, $azureConnector)  
+    }
+}
+
+#Check logic apps to find orphaned connectors
+Write-Host ''
+Write-Host 'Looking up Consumption Logic Apps'
+$resources = Get-AzResource -ResourceGroupName $resourcegroupName -ResourceType Microsoft.Logic/workflows
+$resources | ForEach-Object {    
+    $resourceName = $_.Name    
+    $logicAppName = $resourceName
+    $logicApp = Get-AzLogicApp -Name $logicAppName -ResourceGroupName $resourceGroupName        
+    $logicAppUrl = $resourceGroupPath + '/providers/Microsoft.Logic/workflows/' + $logicApp.Name + '?api-version=2018-07-01-preview'
+    
+    #Get Logic App Content using Az REST GET
+    $logicAppJson = az rest --method get --uri $logicAppUrl
+    $logicAppJsonText = $logicAppJson | ConvertFrom-Json
+    
+    #Check Logic App Connectors inside the Logic App JSON
+    $logicAppParameters = $logicAppJsonText.properties.parameters
+    $logicAppConnections = $logicAppParameters.psobject.properties.Where({$_.name -eq '$connections'}).value
+    $logicAppConnectionValue = $logicAppConnections.value
+    
+    #Iterate through the connectors
+    Write-Host 'Logic App ' $logicAppName 'uses the following connector/s:'
+    $logicAppConnectionValue.psobject.properties | ForEach-Object {
+        $objectName = $_
+        $connection = $objectName.Value
+        if($connection -ne $null)
+        {
+            Write-Host $connection.connectionName '| Is Deprecated? ' ($connection.connectionName.ToString().ToUpper() -contains 'DEPRECATED') ' | id=' $connection.connectionId
+            #Check if connector is in the connector dictionary
+            $connectorIdLower = $connection.connectionId.ToLower()
+            if($connectorDictionary.ContainsKey($connectorIdLower))
+            {
+                #Mark connector as being used                        
+                $matchingConnector = $connectorDictionary[$connectorIdLower]
+                $matchingConnector.IsUsed = 'TRUE'
+                $connectorDictionary[$connectorIdLower] = $matchingConnector 
+            }
+        }                                                    
+    }       
+}
+
+Write-Host ''
+Write-Host 'Orphaned API Connectors'
+$connectorDictionary.Values | ForEach-Object{
+    Write-Host $_.name ': Is Used?' $_.IsUsed
+    if($_.IsUsed -eq 'FALSE')
+    {
+        Write-Host $_.name ': is an orphan | ID:' $_.Id
+    }
+}
+
+$connectorDictionary.Values | Export-Csv -Path $csvFilepath
